3705e2676ab4775039b16a813372db63a5e722c3
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-06-18 10:24:58 +0100

    refactored the election entity

--- src/main/java/africa/semicolon/election_management_system/data/models/Election.java
+++ src/main/java/africa/semicolon/election_management_system/data/models/Election.java
@@ -1,11 +1,14 @@
 package africa.semicolon.election_management_system.data.models;
 
+import africa.semicolon.election_management_system.data.constants.Category;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -17,4 +20,8 @@ public class Election {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    private String title;
+    private Category category;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 }
