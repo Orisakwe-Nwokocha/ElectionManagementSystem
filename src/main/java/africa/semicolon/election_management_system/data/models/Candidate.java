8745da09360d6de660ab8fa15ba40fd7a9b8b7c0
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-06-18 11:08:30 +0100

    update

--- src/main/java/africa/semicolon/election_management_system/data/models/Candidate.java
+++ src/main/java/africa/semicolon/election_management_system/data/models/Candidate.java
@@ -1,11 +1,13 @@
 package africa.semicolon.election_management_system.data.models;
 
 import africa.semicolon.election_management_system.data.constants.Category;
+import africa.semicolon.election_management_system.data.constants.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -25,5 +27,11 @@ public class Candidate {
     private String partyAffiliation;
     private Category positionContested;
     private String identificationNumber;
+    private Integer votingId;
+    private Role role;
+    @ManyToOne
+    private Election election;
+    private LocalDateTime dateRegistered;
+    private LocalDateTime dateUpdated;
 
 }
