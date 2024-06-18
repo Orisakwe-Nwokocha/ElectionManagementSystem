dc6160652062870c630d85023f907861a4463f1d
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-06-18 10:55:00 +0100

    update

--- src/main/java/africa/semicolon/election_management_system/data/models/Candidate.java
+++ src/main/java/africa/semicolon/election_management_system/data/models/Candidate.java
@@ -1,8 +1,11 @@
 package africa.semicolon.election_management_system.data.models;
 
+import africa.semicolon.election_management_system.data.constants.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -14,4 +17,13 @@ public class Candidate {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    private String name;
+    private String password;
+    private String address;
+    private LocalDate dob;
+    private String stateOfOrigin;
+    private String partyAffiliation;
+    private Category positionContested;
+    private String identificationNumber;
+
 }
