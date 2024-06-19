021c120deb8ab85703a470d363ca0f31dd16da54
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-19 15:15:11 +0100

    dummy data

--- src/main/java/africa/semicolon/election_management_system/services/AdminServiceImpl.java
+++ src/main/java/africa/semicolon/election_management_system/services/AdminServiceImpl.java
@@ -1,9 +1,22 @@
 package africa.semicolon.election_management_system.services;
 
 
+import africa.semicolon.election_management_system.dtos.requests.RegisterAdminRequest;
+import africa.semicolon.election_management_system.dtos.requests.ScheduleElectionRequest;
+import africa.semicolon.election_management_system.dtos.responses.RegisterAdminResponse;
+import africa.semicolon.election_management_system.dtos.responses.ScheduleElectionResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
+    @Override
+    public RegisterAdminResponse register(RegisterAdminRequest request) {
+        return null;
+    }
+
+    @Override
+    public ScheduleElectionResponse schedule(ScheduleElectionRequest request) {
+        return null;
+    }
 }
---
+++ src/test/java/africa/semicolon/election_management_system/services/AdminServiceImplTest.java
@@ -0,0 +1,17 @@
+package africa.semicolon.election_management_system.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceImplTest {
+
+    @Test
+    void testThatAdminCanRegister() {
+
+    }
+
+    @Test
+    void schedule() {
+    }
+}
