8497ab84408b6672e9cedda4f1916845fdd2dac2
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-20 18:22:03 +0100

    admin_controller

--- src/test/java/africa/semicolon/election_management_system/controllers/AdminControllerTest.java
+++ src/test/java/africa/semicolon/election_management_system/controllers/AdminControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockPart;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,9 +33,8 @@ class AdminControllerTest {
         mockMvc.perform(post("/api/v1/admin")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(registerAdminRequest)))
-                        .andExpect(status().isCreated())
-                        .andDo(print());
+                .andExpect(status().isCreated())
+                .andDo(print());
     }
-
 
 }
